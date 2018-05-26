6131e34b681ea4e32c39f694f8a9c15f9424ed99
David Sisson <davidfsol5@gmail.com>
2018-05-26 01:27:24 -0400

    FinishedHack
    Proof of Work must be repeated to repair chain validity after a hack.

--- ProofOfWork/BlockchainDemo/Program.cs
+++ ProofOfWork/BlockchainDemo/Program.cs
@@ -8,35 +8,39 @@ namespace BlockchainDemo
         static void Main(string[] args)
         {
             ElapsedTimer timer = new ElapsedTimer();
-            int miningDifficulty = 4;
+            int miningDifficulty = 3;
 
             Blockchain phillyCoin = new Blockchain(miningDifficulty);
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
 
-            Console.WriteLine($"Duration: {timer.ElapsedTime()}");
+            Console.WriteLine($"Three data blocks mined: {timer.ElapsedTime()}");
+            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
 
             Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
 
-            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
-
-            Console.WriteLine($"Update amount to 1000");
             phillyCoin.Chain[1].Data = "{sender:Henry,receiver:MaHesh,amount:1000}";
-
+            Console.WriteLine($"Block 1 hacked. Update amount to 1000: {timer.ElapsedTime()}");
             Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
 
-            Console.WriteLine($"Update hash");
             phillyCoin.Chain[1].Hash = phillyCoin.Chain[1].CalculateHash();
-
+            Console.WriteLine($"Block 1 rehash: {timer.ElapsedTime()}");
             Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
 
-            Console.WriteLine($"Update the entire chain");
             phillyCoin.Chain[2].PreviousHash = phillyCoin.Chain[1].Hash;
             phillyCoin.Chain[2].Hash = phillyCoin.Chain[2].CalculateHash();
             phillyCoin.Chain[3].PreviousHash = phillyCoin.Chain[2].Hash;
             phillyCoin.Chain[3].Hash = phillyCoin.Chain[3].CalculateHash();
+            Console.WriteLine($"Rest of chain rehashed: {timer.ElapsedTime()}");
+            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
 
+            phillyCoin.Chain[1].Mine(phillyCoin.MiningDifficulty);
+            phillyCoin.Chain[2].PreviousHash = phillyCoin.Chain[1].Hash;
+            phillyCoin.Chain[2].Mine(phillyCoin.MiningDifficulty);
+            phillyCoin.Chain[3].PreviousHash = phillyCoin.Chain[2].Hash;
+            phillyCoin.Chain[3].Mine(phillyCoin.MiningDifficulty);
+            Console.WriteLine($"Chain remined from hack to end: {timer.ElapsedTime()}");
             Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
 
             Console.ReadKey();
