0dd16e70ef4a51fb1383afe23aa05f6ded0ca07c
David Sisson <davidfsol5@gmail.com>
2018-05-26 00:06:12 -0400

    Modified ProofOfWork chain validation. Minor fixes to Basic. Some refactoring,

--- Basic/BlockchainDemo/Program.cs
+++ Basic/BlockchainDemo/Program.cs
@@ -10,7 +10,7 @@ namespace BlockchainDemo
             Blockchain phillyCoin = new Blockchain();
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
-            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
 
             Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
 
